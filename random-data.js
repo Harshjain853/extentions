6f523841af193255e2fa9afb8fa793353f8e3d5e
HARSH JAIN <74600304+Harshjain853@users.noreply.github.com>
2024-08-04 10:39:24 +0530

    Update random-data.js

--- random-data.js
+++ random-data.js
@@ -20,26 +20,39 @@ function getRandomText() {
     return date.toISOString().split('T')[0]; // Format date as YYYY-MM-DD
   }
   
-  document.querySelectorAll('input').forEach(input => {
-    switch (input.type) {
-      case 'text':
-        input.value = getRandomText();
-        break;
-      case 'email':
-        input.value = getRandomEmail();
-        break;
-      case 'number':
-        input.value = getRandomNumber();
-        break;
-      case 'date':
-        input.value = getRandomDate();
-        break;
-      case 'checkbox':
-        input.checked = true;
-        break;
-      default:
-        input.value = getRandomText();
-        break;
+  function getRandomPhoneNumber() {
+    let phoneNumber = "9";
+    for (let i = 0; i < 9; i++) {
+      phoneNumber += Math.floor(Math.random() * 10).toString();
     }
+    return phoneNumber;
+  }
+  
+  function getRandomVerificationNumber() {
+    return Math.floor(Math.random() * (9999 - 10 + 1)) + 10; // Random number between 10 and 9999
+  }
+  
+  function fillInputBasedOnNameOrId(element) {
+    const name = element.name.toLowerCase();
+    const id = element.id.toLowerCase();
+    if (name.includes("email")) {
+      element.value = getRandomEmail();
+    } else if (name.includes("date")) {
+      element.value = getRandomDate();
+    } else if (name.includes("number") || name.includes("age")) {
+      element.value = getRandomNumber();
+    } else if (name.includes("phone") || name.includes("contact no") || name.includes("phone no")) {
+      element.value = getRandomPhoneNumber();
+    } else if (id.includes("verif_box") || id.includes("verification")) {
+      element.value = getRandomVerificationNumber();
+    } else if (element.type === "checkbox") {
+      element.checked = true;
+    } else {
+      element.value = getRandomText();
+    }
+  }
+  
+  document.querySelectorAll('input, textarea').forEach(element => {
+    fillInputBasedOnNameOrId(element);
   });
   
